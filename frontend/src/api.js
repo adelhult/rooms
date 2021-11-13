e5c97c699d0b40a890bb1e410a592e5d85536d04
Leopold Wigbratt <36993054+leopoldwigbratt@users.noreply.github.com>
2021-11-13 21:48:02 +0100

    Added  API client api.js

---
+++ frontend/src/api.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import config from 'config';
+
+const axiosInstance = axios.create({
+  baseURL: config.appBaseUrl,
+  timeout: 2000,
+  validateStatus: status => status === 200,
+});
+
+export const getRoomInfo = async roomName => new Promise((resolve, reject) => {
+  axiosInstance.get(`/info/${roomName}`).then(response => {
+    resolve(response.data);
+  }).catch(error => {
+    reject(error);
+  });
+});
+
+export const getSuggestions = async (options = {}) => new Promise((resolve, reject) => {
+  axiosInstance.get('/suggestions', {
+    params: {
+      number: options.number ?? 4,
+      minSeats: options.minSeats ?? 4,
+      minTime: options.minTime ?? 45,
+      from: options.from ?? Date.now(),
+      equipment: options.equipment ?? [],
+    }
+  }).then(response => {
+    resolve(response.data);
+  }).catch(error => {
+    reject(error);
+  });
+});
