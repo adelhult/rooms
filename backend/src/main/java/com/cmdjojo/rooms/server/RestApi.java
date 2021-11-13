5b7edf05c512d0eda999e1da8ef712f5e0455cbc
Edvin Nilsson <edvinnil@student.chalmers.se>
2021-11-13 23:02:59 +0100

    Changes by Eli

--- backend/src/main/java/com/cmdjojo/rooms/server/RestApi.java
+++ backend/src/main/java/com/cmdjojo/rooms/server/RestApi.java
@@ -22,6 +22,24 @@ public class RestApi {
 
     private static final int port = 8080;
 
+    private static int parseOr(String s, int defaultValue) {
+        if (s == null) return defaultValue;
+        try {
+            return Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            return defaultValue;
+        }
+    }
+
+    private static long parseOr(String s, long defaultValue) {
+        if (s == null) return defaultValue;
+        try {
+            return Long.parseLong(s);
+        } catch (NumberFormatException e) {
+            return defaultValue;
+        }
+    }
+
     public static void start() {
         Javalin app = Javalin.create().start(port);
         app.get("/info/{roomName}", ctx -> {
@@ -29,6 +47,59 @@ public class RestApi {
             var info = DataCacher.getRoomInfo(roomName);
             ctx.result(gson.toJson(info));
         });
+
+        app.get("/suggestions", ctx -> {
+            // get params or else set default values
+            int number = parseOr(ctx.queryParam("number"), 1);
+            int minSeats = parseOr(ctx.queryParam("minSeats"), 0);
+            int minTimeInt = parseOr(ctx.queryParam("minTime"), 15);
+            long from = parseOr(ctx.queryParam("from"), System.currentTimeMillis());
+            Instant fromDateTime = Instant.ofEpochMilli(from);
+            Duration minTime = Duration.of(minTimeInt, ChronoUnit.MINUTES);
+
+            String[] equipment = Objects.requireNonNullElse(ctx.queryParam("equipment"), "")
+                            .split(",");
+
+            List<RoomDataSent> sortedRooms = DataCacher.getRooms()
+                    .values()
+                    .stream()
+                    .filter(room -> {
+                        var roomInfo = DataCacher.getRoomInfo(room.name);
+                        try {
+                            // check minSeats
+                            if (roomInfo.roomSeats < minSeats) {
+                                return false;
+                            }
+
+                            for (var thing : equipment) {
+                                if (!roomInfo.equipment.contains(thing))
+                                    return false;
+                            }
+
+                        } catch (Exception e) {
+                            return false;
+                        }
+                        return true;
+                    })
+                    .sorted(Comparator.comparing(room -> room.getTimeUntilFree(fromDateTime, minTime)))
+                    .limit(number)
+                    .map(room -> new RoomDataSent(room, fromDateTime, minTime))
+                    .collect(Collectors.toList());
+
+            ctx.contentType("application/json");
+            ctx.result(gson.toJson(sortedRooms));
+        });
+
+    }
+
+    static class RoomDataSent {
+        String name;
+        Room.TimeSlot timeslot;
+
+        RoomDataSent(Room room, Instant fromDateTime, Duration minTime) {
+            name = room.name;
+            timeslot = room.getNextFreeSlot(fromDateTime, minTime);
+        }
     }
 
 }
