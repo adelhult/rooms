59f35ac876aa0c74d784af9fb3895d8fa235c5a2
Jonathan Widén <jonathan.loke.widen@gmail.com>
2021-11-13 19:03:05 +0100

    Some more stuff added, like priorities to rooms

--- backend/src/main/java/com/cmdjojo/rooms/server/Main.java
+++ backend/src/main/java/com/cmdjojo/rooms/server/Main.java
@@ -1,17 +1,34 @@
 package com.cmdjojo.rooms.server;
 
-import biweekly.ICalendar;
 import com.cmdjojo.rooms.core.DataCacher;
+import com.cmdjojo.rooms.structs.Room;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String... args) {
         DataCacher.cacheNewInstantly();
-        System.out.println("abc");
-        ICalendar cal = Downloader.cal();
-        System.out.println(cal.getEvents().size());
-        
-        cal.getEvents().forEach(e -> System.out.printf("%s TO %s IN %s%n", e.getDateStart().getValue(),
-                e.getDateEnd().getValue(),
-                e.getLocation().getValue()));
+        var map = DataCacher.getRooms();
+        assert map != null;
+        var sim = LocalDateTime.of(2021, 11, 17, 12, 20, 0, 0).toInstant(ZoneOffset.ofHours(0));
+        System.out.printf("%d rooms found: %s%n", map.size(), String.join(", ", map.keySet()));
+        map.values().forEach(room -> room.bookings.sort(Comparator.comparing(Room.TimeSlot::getStart)));
+        for (Room value : map.values()) {
+            System.out.printf("Room %s has %d bookings:%n", value.name, value.bookings.size());
+            value.bookings.forEach(System.out::println);
+            System.out.println("It is free in " + value.getTimeUntilFree(sim));
+        }
+        System.out.println("Time is " + sim.toString());
+
+
+//        System.out.println("abc");
+//        ICalendar cal = Downloader.cal();
+//        System.out.println(cal.getEvents().size());
+//
+//        cal.getEvents().forEach(e -> System.out.printf("%s TO %s IN %s%n", e.getDateStart().getValue(),
+//                e.getDateEnd().getValue(),
+//                e.getLocation().getValue()));
     }
 }
--- backend/src/main/java/com/cmdjojo/rooms/structs/Room.java
+++ backend/src/main/java/com/cmdjojo/rooms/structs/Room.java
@@ -1,24 +1,34 @@
 package com.cmdjojo.rooms.structs;
 
+import biweekly.component.VEvent;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import java.time.Duration;
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Objects;
+import java.util.regex.Pattern;
 
 public class Room {
-    public String name;
-    public ArrayList<TimeSlot> bookings;
+    public @NotNull String name;
+    public @NotNull ArrayList<TimeSlot> bookings;
+    private final int prioOrdinal;
+    private transient Priority priority; // do not serialize
     
-    boolean isOccupiedAt(Instant d) {
+    public Room(@NotNull String name) {
+        this.name = name;
+        bookings = new ArrayList<>();
+        priority = Priority.search(name);
+        prioOrdinal = priority.ordinal();
+    }
+    
+    public boolean isOccupiedAt(@NotNull Instant d) {
         return bookingAt(d) != null;
     }
     
-    public Room(String name) {
-        this.name = name;
-        bookings = new ArrayList<>();
-    }
-    
-    TimeSlot bookingAt(Instant d) {
+    public @Nullable TimeSlot bookingAt(@NotNull Instant d) {
         Objects.requireNonNull(d);
         return bookings.stream()
                 .filter(booking -> booking.start.isBefore(d) && booking.end.isAfter(d))
@@ -26,7 +36,7 @@ public class Room {
                 .orElse(null);
     }
     
-    TimeSlot getNextFreeSlot(Instant d) {
+    public @NotNull TimeSlot getNextFreeSlot(Instant d) {
         TimeSlot current = bookingAt(d);
         Instant start = current == null ? d : current.end;
         Instant end = bookings.stream()
@@ -34,32 +44,99 @@ public class Room {
                 .filter(bookingStart -> bookingStart.isAfter(start) || bookingStart.equals(start))
                 .sorted()
                 .findFirst()
-                .orElse(null);
+                .orElse(d.plus(10, ChronoUnit.DAYS));
         return new TimeSlot(start, end);
     }
     
+    public @NotNull Priority getPriority() {
+        if (priority == null) priority = Priority.values()[prioOrdinal];
+        return priority;
+    }
     
-    Duration getTimeUntilFree(Instant d) {
+    
+    public @NotNull Duration getTimeUntilFree(Instant d) {
         TimeSlot current = bookingAt(d);
         if (current == null) return Duration.ZERO;
         else return Duration.between(d, current.end);
     }
     
+    @Override
+    public String toString() {
+        return "Room{" +
+                "name='" + name + '\'' +
+                ", priority='" + priority.toString() + '\'' +
+                ", bookings=" + bookings +
+                '}';
+    }
+
+//    public static int getPriority(String name) {
+//        if (name.startsWith("EG-2")) {
+//            return 1;
+//        }
+//    }
     
-    static class TimeSlot {
-        public Instant start, end;
+    public enum Priority {
+        NC_FLOOR_1("EG-251[56]"),
+        NC_FLOOR_2("EG-350[3-8]"),
+        EDIT_FLOOR_3("EG-3.+"),
+        EDIT_FLOOR_4("EG-4.*"),
+        EDIT_FLOOR_5("EG-5.*"),
+        EDIT_FLOOR_6("EG-6.*"),
+        UNKNOWN(".*");
         
-        public TimeSlot(Instant start, Instant end) {
+        private final Pattern pattern;
+        public final double timeMultiplier;
+        
+        Priority(String regex) {
+            pattern = Pattern.compile(regex);
+            timeMultiplier = Math.pow(1.1, ordinal());
+        }
+        
+        static Priority search(String name) {
+            for (Priority value : values()) {
+                if (value.pattern.matcher(name).matches()) return value;
+            }
+            return UNKNOWN;
+        }
+    }
+    
+    public static class TimeSlot {
+        
+        public final @NotNull Instant start;
+        public final @NotNull Instant end;
+        
+        public TimeSlot(VEvent event) {
+            this(event.getDateStart().getValue().toInstant(), event.getDateEnd().getValue().toInstant());
+        }
+        
+        public TimeSlot(@NotNull Instant start, @NotNull Instant end) {
             this.start = start;
             this.end = end;
         }
         
-        public Instant getStart() {
+        public @NotNull Instant getStart() {
             return start;
         }
         
-        public Instant getEnd() {
+        public @NotNull Instant getEnd() {
             return end;
+        }
+        
+        public @NotNull Duration getDuration() {
+            return Duration.between(start, end);
+        }
+        
+        public @NotNull Duration timeUntilStart(Instant now) {
+            if (start.isBefore(now)) return Duration.ZERO;
+            else return Duration.between(now, start);
+        }
+        
+        @Override
+        public String toString() {
+            return "TimeSlot{" +
+                    "start=" + start +
+                    ", end=" + end +
+                    '}';
         }
     }
 }
